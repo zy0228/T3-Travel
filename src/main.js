c11ae423444a31c1aa047f5f1c09ab77fe82da28
zy0228 <2472305432@qq.com>
2020-10-29 09:38:27 +0800

    feat:  ohbug monitor

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Ohbug from '@ohbug/browser'
+import OhbugVue from '@ohbug/vue'
+
 // import fastclick from 'fastclick'
 // import VConsole from 'vconsole'
 
@@ -11,6 +14,12 @@ Vue.config.productionTip = false
 // const vConsole = new VConsole()
 
 // fastclick.attach(document.body)
+
+const client = Ohbug.init({
+  apiKey: '348334ee85820d11d9dbd0c571208475b9d57230e0c34ea6cedfc9506f6454e4'
+})
+
+client.use(OhbugVue, Vue)
 
 new Vue({
   router,
